9fd3d76998df95e88e79254c08d49b51ea8236f4
Kenneth Nordahl <kenneth@nordahl.me>
2011-12-03 18:04:20 -0800

    Separated out the routes to a separate file, added content_for helper.
    Signed-off-by: Kenneth Nordahl <kenneth@nordahl.me>

--- app.rb
+++ app.rb
@@ -1,23 +1,22 @@
 require 'bundler'
 Bundler.require
+require 'sinatra/content_for'
 
+# Set standard views path
+set :views, settings.root + '/app/views'
 
-#ROOT_DIR = $0 unless defined? ROOT_DIR
-
+# Define root path
 ROOT_DIR = File.expand_path(File.dirname(__FILE__))
-
 def root_path(*args)
   File.join(ROOT_DIR, *args)
 end
-
 
 # Load app files
 Dir[root_path("app/*.rb")].each do |file|
   require file
 end
 
-
-
-get '/', :provides => 'html' do
-	haml :index
+# Load config files
+Dir[root_path("config/*.rb")].each do |file|
+  require file
 end
